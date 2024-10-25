bb3542f2ea220a0d33bc0f29384bf793d1031f91
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-25 11:54:38 -0400

    initial page setup

---
+++ index.js
@@ -0,0 +1,10 @@
+//on the client side. we will have an input to get the user's input and a button they can click
+//when they enter a string that string will be posted to the backend and get a response back 
+
+//in the backend
+//ROUTES : 
+// get home route: we serve the index.html
+//post home route that takes query params /?word=inputword which returns with the response
+
+//in index.js we get the value from the input and send the post request to the backend
+ 
