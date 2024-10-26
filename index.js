34ad56fec0921887c7233379467d765558e6058f
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-25 21:11:56 -0400

    add a checkPali eventListener to the form

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 //on the client side. we will have an input to get the user's input and a button they can click
-//when they enter a string that string will be posted to the backend and get a response back 
+//when they enter a string that string will be posted to the backend and get a response back
 
-//in the backend
-//ROUTES : 
-// get home route: we serve the index.html
-//post home route that takes query params /?word=inputword which returns with the response
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => checkPali(e));
 
-//in index.js we get the value from the input and send the post request to the backend
- 
+function checkPali(e) {
+  e.preventDefault();
+  const word = document.querySelector('#word').value;
+  const url = `/api?word=${word}`;
+
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((res) => {
+      document.querySelector('#result').textContent = `${word} ${res.result ? 'is' : 'IS NOT'} a palindrome `; 
+    })
+    .catch((err) => console.log(err));
+}
+
